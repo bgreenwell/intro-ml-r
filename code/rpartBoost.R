95b09ea5c9707f398b8889cff047ad498fccb32d
bgreenwell <greenwell.brandon@gmail.com>
2018-10-30 14:12:12 -0400

    update content

--- code/rpartBoost.R
+++ code/rpartBoost.R
@@ -13,7 +13,8 @@ rpartBoost <- function(X, y, data, num_trees = 100, learn_rate = 0.1,
     if (verbose) {
       message("iter ", tree, " of ", num_trees)
     }
-    g_b_tilde <- rpart(r ~ X, control = list(cp = 0, maxdepth = tree_depth))
+    g_b_tilde <- rpart(r ~ X, control = list(cp = 0, maxdepth = tree_depth,
+                                             minsplit = 2))
     g_b_hat <- learn_rate * predict(g_b_tilde)
     G_b_hat[, tree + 1] <- G_b_hat[, tree] + matrix(g_b_hat)
     r <- r - g_b_hat
@@ -41,11 +42,11 @@ plot(x, y)
 
 # gradient boosted decision trees
 bst <- rpartBoost(X = x, y = y, num_trees = 1000, learn_rate = 0.1, 
-                  tree_depth = 3, verbose = TRUE)
+                  tree_depth = 1, verbose = TRUE)
 
 # Plot first 15 iterations
-par(mfrow = c(3, 3))
-for (i in c(0, 5, 10, 15, 25, 50, 100, 500, 1000)) {
+par(mfrow = c(4, 4))
+for (i in c(0:8, 10, 15, 25, 50, 100, 500, 1000)) {
   plotIter(bst, iter = i, main = paste("Iter:", i))
 }
 
